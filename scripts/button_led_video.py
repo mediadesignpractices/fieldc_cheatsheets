2216d6d06ef229cd1fce5951bd733c53172e88f3
caseyanderson <cta@caseyanderson.com>
2017-10-10 11:14:21 -0700

    button led video script

---
+++ scripts/button_led_video.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+# press and hold to record video
+
+from gpiozero import Button
+from gpiozero import LED
+from picamera import PiCamera
+from datetime import datetime
+from time import sleep
+
+# setup led and button
+button = Button(2)
+led = LED(17)
+
+# recording flag
+is_recording = 0
+
+try:
+
+    while True:
+        if (button.value == True) and (is_recording == 0):
+
+            camera = PiCamera()
+            sleep(2)
+            camera.start_preview()
+            print("CALIBRATING...")
+            print("READY TO RECORD!")
+            print()
+            print("RECORDING")
+            x = datetime.now().strftime('%Y-%m-%d-%H-%m-%s')
+            path = ''.join(['/home/pi/', x, '.h264'])
+            camera.start_recording(path)
+            is_recording = 1
+            led.on()
+
+        elif (button.value == False) and (is_recording == 1):
+            print("DONE RECORDING!")
+            camera.stop_recording()
+            camera.stop_preview()
+            led.off()
+            camera.close()
+            is_recording = 0
+
+
+except KeyboardInterrupt:
+    print("INTERRUPTED!")
+    button.close()
+    camera.close()
+    led.close()
